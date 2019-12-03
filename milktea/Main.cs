c68a21c1a8ea0cc0ebcafef4d4b1be1104b533bc
hanabi0412 <nguyenkientrung0412@gmail.com>
2019-12-04 03:20:00 +0700

    Phân chia dõ dàng tính năng

--- milktea/Main.cs
+++ milktea/Main.cs
@@ -10,6 +10,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using WindowsFormsApp4.Entity;
 using WindowsFormsApp4.Imp;
+using milktea.Model;
 
 namespace milktea
 {
@@ -18,33 +19,9 @@ namespace milktea
         public Form1()
         {
             InitializeComponent();
-            List<Table> tables = TableDAO.Show();
-            foreach (var table in tables)
-            {
-                Button btn = new Button();
-                String nameBtn = String.Format("Bàn {0}", table.Id);
-                btn.Name = table.Id;
-                btn.Text = nameBtn;
-                btn.Font = new Font("Segoe UI", 14F);
-                switch (table.Status)
-                {
-                    case 1:
-                        btn.BackColor = Color.Chartreuse;
-                        break;
-                    case 2:
-                        btn.BackColor = Color.Firebrick;
-                        break;
-                    case 3:
-                        btn.BackColor = Color.Goldenrod;
-                        break;
-                    case 4:
-                        btn.BackColor = Color.Gray;
-                        break;
-                    
-                }
-                btn.Size = new Size(100, 50);
-                flowLayoutPanel1.Controls.Add(btn);
-            }
+            
+            //Chỉ cần chuyền tên flowLayoutPanel là tất cả auto
+            TableModel.ShowIcon(flowLayoutPanel1);
         }
     }
 }
---
+++ milktea/Model/TableModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using WindowsFormsApp4.Entity;
+using WindowsFormsApp4.Imp;
+
+namespace milktea.Model
+{
+    public class TableModel
+    {
+        public static void ShowIcon(FlowLayoutPanel flowLayoutPanel)
+        {
+            List<Table> tables = TableDAO.Show();
+            foreach (var table in tables)
+            {
+                Button btn = new Button();
+                String nameBtn = String.Format("Bàn {0}", table.Id);
+                btn.Name = table.Id;
+                btn.Text = nameBtn;
+                btn.Font = new Font("Segoe UI", 14F);
+                switch (table.Status)
+                {
+                    case 1:
+                        btn.BackColor = Color.Chartreuse;
+                        break;
+                    case 2:
+                        btn.BackColor = Color.Firebrick;
+                        break;
+                    case 3:
+                        btn.BackColor = Color.Goldenrod;
+                        break;
+                    case 4:
+                        btn.BackColor = Color.Gray;
+                        break;
+
+                }
+
+                btn.Size = new Size(100, 50);
+                flowLayoutPanel.Controls.Add(btn);
+            }
+        }
+    }
+}
